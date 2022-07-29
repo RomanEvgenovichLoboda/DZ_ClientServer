a29188c9cbd8967de19c6b1a1cdf4e3b7bac73b2
loboda.rom@gmail.com <loboda.rom@gmail.com>
2022-07-29 17:40:28 +0300

    final

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -7,38 +7,36 @@ namespace Client
     {
         const int PORT = 8088;
         const string IP = "127.0.0.1";
-       
         IPEndPoint iPEnd = new IPEndPoint(IPAddress.Parse(IP), PORT);
         Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         public Form1()
         {
             InitializeComponent();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
-           
-            if(textBox1.Text != "")
+            try
             {
-                byte[] data = Encoding.Unicode.GetBytes(textBox1.Text);
-                clientSocket.Send(data);
-                int bytes = 0;
-                byte[] buffer = new byte[1024];
-                StringBuilder builder = new StringBuilder();
-                do
+                if (textBox1.Text != "")
                 {
-                    bytes = clientSocket.Receive(buffer);
-                    builder.Append(Encoding.Unicode.GetString(buffer, 0, bytes));
-                } while (clientSocket.Available > 0);
-                label2.Text = builder.ToString();
-
-
+                    byte[] data = Encoding.Unicode.GetBytes(textBox1.Text);
+                    clientSocket.Send(data);
+                    int bytes = 0;
+                    byte[] buffer = new byte[1024];
+                    StringBuilder builder = new StringBuilder();
+                    do
+                    {
+                        bytes = clientSocket.Receive(buffer);
+                        builder.Append(Encoding.Unicode.GetString(buffer, 0, bytes));
+                    } while (clientSocket.Available > 0);
+                    label2.Text = builder.ToString();
+                }
             }
-
-
-
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             clientSocket.Connect(iPEnd);
--- DZ_ClientServer/Program.cs
+++ DZ_ClientServer/Program.cs
@@ -4,19 +4,16 @@ using System.Text;
 
 const int PORT = 8088;
 const string IP = "127.0.0.1";
-
 Console.WriteLine("Server start...");
 IPEndPoint iPEnd = new IPEndPoint(IPAddress.Parse(IP), PORT);
 Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 try
 {
-
     serverSocket.Bind(iPEnd);
     serverSocket.Listen(10);
     Socket clientSocket = serverSocket.Accept();
     do
     {
-
         Console.WriteLine($"Server listen on {IP}:{PORT}");
         int bytes = 0;
         byte[] buffer = new byte[1024];
@@ -26,32 +23,55 @@ try
             bytes = clientSocket.Receive(buffer);
             builder.Append(Encoding.Unicode.GetString(buffer, 0, bytes));
         } while (clientSocket.Available > 0);
-
-
-        int summ = 0;
-        foreach (var item in builder.ToString())
+        Console.WriteLine($"Equation:\t{builder.ToString()}");
+        try
         {
-            //if(item == '0')
-            //{
-            //    summ *= 10;
-            //}
-            summ += int.Parse(item.ToString());
+            string number1 = "";
+            string number2 = "";
+            string symbol = "";
+            double result = 0;
+            foreach (var item in builder.ToString())
+            { 
+                if (item.ToString() == "*" || item.ToString() == "/" || item.ToString() == "+" || item.ToString() == "-") { symbol = item.ToString(); }
+                else if (symbol == "") { number1 += item.ToString(); }
+                else if (symbol != "" && number1 != "") { number2 += item.ToString(); }
+            }
+            switch (symbol)
+            {
+                case "*":
+                    {
+                        result = double.Parse(number1) * double.Parse(number2);
+                        break;
+                    }
+                case "/":
+                    {
+                        result = double.Parse(number1) / double.Parse(number2);
+                        break;
+                    }
+                case "+":
+                    {
+                        result = double.Parse(number1) + double.Parse(number2);
+                        break;
+                    }
+                case "-":
+                    {
+                        result = double.Parse(number1) - double.Parse(number2);
+                        break;
+                    }
+                default:
+                    break;
+            }
+            byte[] data = Encoding.Unicode.GetBytes(result.ToString());
+            clientSocket.Send(data);
         }
-
-
-
-        Console.WriteLine($"client:\t{builder.ToString()}");
-
-        byte[] data = Encoding.Unicode.GetBytes(summ.ToString());
-
-        clientSocket.Send(data);
-
-
-
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            byte[] data = Encoding.Unicode.GetBytes("Boroda");
+            clientSocket.Send(data);
+        }
     } while (true);
     clientSocket.Close();
-
-
     serverSocket.Shutdown(SocketShutdown.Both);
     serverSocket.Close();
 }
@@ -59,8 +79,5 @@ catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
 }
-
-
 Console.WriteLine("Server end...");
-
 Console.ReadLine();
